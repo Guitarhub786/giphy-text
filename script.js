64b0d65f300d9aed061044e3d67e1dd0005f30a2
Guitarhub786 <codewithdrawal@gmail.com>
2020-07-27 20:54:25 +0100

    init commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let button = document.querySelector("#getData")
 let message = document.querySelector("#message")
-document.querySelector("#message").innerHTML = "A message to you rudy : "
+document.querySelector("#message").innerHTML = "Type in your message: "
 
 let input = document.getElementById("input");
 input.addEventListener("keyup", function (event) {
@@ -11,64 +11,56 @@ input.addEventListener("keyup", function (event) {
 });
 
 button.addEventListener("click", () => {
-  sendApiRequest()
+  parseText()
+  // sendApiRequest()
 })
 
 
-//fetch data from the API
-function sendApiRequest() {
+function parseText() {
   const count = 25;
   const rnd = Math.random()
   let ping = Math.round(rnd * count)
 
+  let collection = []
   let userInput = document.querySelector("#input").value
-  const responce = fetch(`https://api.giphy.com/v1/gifs/search?api_key=bKgN9fTDyt4siFR46DXdpXAONqESQz4v&q=${userInput}&limit=20&offset=${ping}&rating=G&lang=en`);
-  // console.log(giphy)
+  let parse = userInput.split(" ")
 
-  responce
-    .then(data => data.json())
-    .then(data => {
-      console.log(data)
-      console.log(data.pagination.count)
-
-      if (data.pagination.count === 0) {
-        let log = `[Stopping program] Your input request is not valid! count = ${data.pagination.count}`
-        document.querySelector("#message").innerHTML = log
-        console.log(log)
-      } else {
-        log = `[Found pictures] I will send them now. count =  ${data.pagination.count}`
-        document.querySelector("#message").innerHTML = log
-        console.log(log)
-        useApiData(data)
-      }
-    })
-};
+  for (let i = 0; i < parse.length; i++) {
+    let uber = fetch(`https://api.giphy.com/v1/gifs/search?api_key=bKgN9fTDyt4siFR46DXdpXAONqESQz4v&q=${parse[i]}&limit=${count + 1}&offset=2&rating=G&lang=en`);
 
 
-//do something with the API data you've received.
-function useApiData(gifs) {
-  let all = []
-  document.querySelector("#message").innerHTML = gifs.data.length
+    uber
+      .then(data => data.json())
+      .then(data => {
+        console.log(data)
+        console.log(data.pagination.count)
 
-  console.log(gifs)
+        if (data.pagination.count === 0) {
+          let log = `[Try again!] Could not find a match! count = ${data.pagination.count}`
+          //document.querySelector("#message").innerHTML = log
+          console.log(log)
+        } else {
+          log = `[Found pictures] I will send them now. count =  ${data.pagination.count}`
+          //document.querySelector("#message").innerHTML = log
+          console.log("PASSED", log)
+          console.log("PASSED", parse[i])
+
+          collection.push(`<div id="container">`)
+          collection.push(`<h1 id="usertext">${parse[i]}</h1>`)
+          collection.push(`<img src="${data.data[ping].images.original.url}" alt="missing image">`)
 
 
-  for (let i = 0; i < gifs.data.length; i++) {
-    all.push(`<div id="container">`)
-    all.push(`<img src="${gifs.data[i].images.original.url}">`)
-    all.push(`<h2>${gifs.data[i].title}</h2>`)
-    // all.push(`<p>${gifs.data[i].embed_url}</p>`)
-    all.push(`<a a href = ${gifs.data[i].embed_url} > ${gifs.data[i].embed_url} </a>`)
-    all.push(`</div>`)
+          document.querySelector("#wrapper").innerHTML = collection.join(" ")
+          console.log("Collection", collection)
+
+
+        }
+      })
 
 
   }
-  document.querySelector("#wrapper").innerHTML = all.join("")
 
+  // document.querySelector("#message").innerHTML = collection.join(" ")
+  // console.log(collection)
 
-  console.log("foo", all)
-  let str = all.join("")
-  console.log("bar", str)
-  // document.querySelector("#message").innerHTML = all.length
 }
-
