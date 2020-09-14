cb44a7eebfd9d524be1b59536968fe02cdfdb6b8
Guitarhub786 <codewithdrawal@gmail.com>
2020-09-14 06:58:03 +0100

    update API_KEY

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function parseText() {
   let parse = userInput.split(" ")
 
   for (let i = 0; i < parse.length; i++) {
-    let uber = fetch(`https://api.giphy.com/v1/gifs/search?api_key=bKgN9fTDyt4siFR46DXdpXAONqESQz4v&q=${parse[i]}&limit=${count + 1}&offset=${ping2}&rating=G&lang=en`);
+    let uber = fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${parse[i]}&limit=${count + 1}&offset=${ping2}&rating=G&lang=en`);
 
     uber
       .then(data => data.json())
